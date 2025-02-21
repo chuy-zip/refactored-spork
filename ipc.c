13043a7542a6df5521075a9789ce42363301b636
chuy-zip <andreschuy03@gmail.com>
2025-02-21 15:43:42 -0600

    done ipc

--- ipc.c
+++ ipc.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>    // el wait
 #include <fcntl.h>       //open, O_CREAT, O_RDWR, etc.
 #include <sys/mman.h>    //lo relcionado a memoria compartida mmap, shm_open, shm_unlink
 #include <sys/stat.h>    // modos de permisos (S_IRUSR, S_IWUSR, etc.)
@@ -37,6 +38,8 @@ int main(int argc, char** argv) {
     if(shm_file_descriptor == -1) {
       printf("error al crear la memoria compartida\n");
       return 1;
+    } else {
+      printf("Memorio compartida abierta/creada. File descriptor es: %d \n", shm_file_descriptor);
     }
     
     // configurar tamanio memoria
@@ -45,6 +48,8 @@ int main(int argc, char** argv) {
     if (truncate == -1) {
       printf("Error al configurar tamanio memroria\n");
       return 1;
+    } else {
+      printf("Sized Memoria compartida configurado correctamente a %d bytes.\n", SHM_SIZE);
     }
     
     // aqui hay muchos parametros
@@ -57,14 +62,22 @@ int main(int argc, char** argv) {
     if (ptr == MAP_FAILED) {
         printf("Error al mapear memoria \n");
         return 1;
+    } else {
+        printf("Memoria compartida mapeada con Puntero: %p\n", (void*)ptr);
     }
     
     // ahora los pipes
     int pipefd[2];
+    
+    // pipefd[0] es lectura
+    // pipefd[1] es escritura
+    
     int pipes = pipe(pipefd);
     if (pipes == -1) {
         printf("no se pudo crear un pipe \n");
         return 1;
+    } else {
+        printf("Pipe creado. Lectura: %d, Escritura: %d\n", pipefd[0], pipefd[1]);
     }
 
     // Crear un proceso hijo
@@ -75,9 +88,44 @@ int main(int argc, char** argv) {
     } 
     
     if (pid == 0) {
-        printf("hijo creado, tiene PID %d, y el padre es %d \n", getpid(), getppid());    
+        printf("hijo creado, tiene PID %d, y el padre es %d \n", getpid(), getppid());  
+        
+        // el hijo no va a escribir, solo leera la memoria compartida
+        close(pipefd[1]); // cerrar extremo de escritura
+        
+        char buffer;
+        int index = 0;
+        
+        // leemos con la extremo de lectura del pipe, leemos con un buffer 1 byte
+        while (read(pipefd[0], &buffer, 1) > 0) {
+            ptr[index++] = buffer;  // modificar memoria
+        }
+
+        close(pipefd[0]);  // Cerrar el extremo de lectura una vez termine de leer
+        exit(0);
+        
     } else {
       printf("soy el padre con pid: %d \n", getpid());
+      
+      //el padre solo escribe, no lee
+      close(pipefd[0]);  // Cerrar el extremo de lectura del pipe al principio
+
+    for (int i = 0; i < SHM_SIZE; i++) {
+        if (i % n == 0) {
+            write(pipefd[1], &x, 1);  // Enviar x al hijo
+        }
+    }
+
+      close(pipefd[1]);  // Cerrar el extremo escritura
+      wait(NULL);
+
+      // Mostrar mem compartida
+      printf("La memoria compartida: %s\n", ptr);
+
+      // Liberar
+      munmap(ptr, SHM_SIZE);
+      close(shm_file_descriptor);
+      shm_unlink(shm_name);
     }
 
     
